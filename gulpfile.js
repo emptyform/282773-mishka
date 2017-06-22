0021781469a39fc0861fd15a361a7b4cbfdf41f3
Jamik Karimov <jkarimov1593@gmail.com>
2017-06-22 21:34:43 +0300

    Поправил отсупы

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,60 @@
-"use strict";
+'use strict';
+// Подключаем плагин(модуль)
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');// Группирует медиазапросы и помещает их в конец CSS документа.
+var server = require('browser-sync').create();
+// var minify = require('gulp-csso');
+// var rename = require('gulp-rename');
+// var imagemin = require('gulp-imagemin');
+// var svgstore = require('gulp-svgstore');
+// var svgmin = require('gulp-svgmin');
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
 
-gulp.task("style", function() {
-  gulp.src("sass/style.scss")
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer({browsers: [
-        "last 2 versions"
-      ]})
-    ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+// Описание тасков
+gulp.task('style', function() {
+    gulp.src('sass/style.scss') // Над этим файлом будет работа
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer({browsers: [
+                'last 2 versions'
+            ]}),
+            mqpacker({
+                sort: true
+            })
+        ]))
+        // .pipe(gulp.dest('css'))// (pipe - передача файла от одного интрумента к другому, dest - путь назначения)
+        // .pipe(minify())
+        // .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('css')) // Выплевываем файл который получился, в папку css
+        .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
-  server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
+// gulp.task('images', function() {
+//     return gulp.src('img/**/*.{png,jpg,gif}')
+//         .pipe(imagemin([
+//             imagemin.optipng({optimizationLevel: 3}),
+//             imagemin.jpegtran({progressive: true})
+//         ]))
+//         .pipe(gulp.dest('img'));
+// });
 
-  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+
+// Запуск локального сервера
+gulp.task('serve', ['style'], function() {
+    server.init({
+        server: '.',
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
+
+    // Следит за изменениями файлов
+    gulp.watch('sass/**/*.{scss,sass}', ['style']);
+    gulp.watch('*.html').on('change', server.reload);
 });
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*-----------------------------------------------------------------------------------
+/*  -----------------------------------------------------------------------------------
                                           DROPDOWN
 -------------------------------------------------------------------------------------*/
 (function() {
@@ -9,126 +9,122 @@
     navMain.classList.remove('main-nav--nojs');
 
     navToggle.addEventListener('click', function() {
-      if (navMain.classList.contains('main-nav--closed')) {
-        navMain.classList.remove('main-nav--closed');
-        navMain.classList.add('main-nav--opened');
-      } else {
-        navMain.classList.add('main-nav--closed');
-        navMain.classList.remove('main-nav--opened');
-      }
+        if (navMain.classList.contains('main-nav--closed')) {
+            navMain.classList.remove('main-nav--closed');
+            navMain.classList.add('main-nav--opened');
+        } else {
+            navMain.classList.add('main-nav--closed');
+            navMain.classList.remove('main-nav--opened');
+        }
     });
 
 })();
-/*===============================================================================================================
+/*  ===============================================================================================================
                                                 MODAL
 ================================================================================================================*/
 (function() {
 
-    var modal = document.getElementById("popup");
-    var modalBtnShow = document.querySelector(".featured__btn");
-    var modalBtnHide = document.querySelector(".modal__btn");
-    var wrapper = document.querySelector(".wrapper");
-    var basket = document.querySelectorAll(".product-card__basket");
+    var modal = document.getElementById('popup');
+    var modalBtnShow = document.querySelector('.featured__btn');
+    var modalBtnHide = document.querySelector('.modal__btn');
+    var wrapper = document.querySelector('.wrapper');
+    var basket = document.querySelectorAll('.product-card__basket');
 
-      function creatOverlay() {
+    function creatOverlay() {
         var docHeight = document.body.offsetHeight;
-        wrapper.classList.add("wrap-visible");
-        wrapper.style.height = docHeight + "px";
-      }
 
-      function showModal(event) {
+        wrapper.classList.add('wrap-visible');
+        wrapper.style.height = docHeight + 'px';
+    }
+
+    function showModal(event) {
         event.preventDefault();
-
         creatOverlay();
-        modal.classList.add("modal--visible");
-      }
+        modal.classList.add('modal--visible');
+    }
 
-      function hideModal(event) {
+    function hideModal(event) {
         event.preventDefault();
-        modal.classList.remove("modal--visible");
-        wrapper.classList.remove("wrap-visible")
+        modal.classList.remove('modal--visible');
+        wrapper.classList.remove('wrap-visible');
         wrapper.style.height = 0;
-      }
+    }
 
-      if (modalBtnShow && wrapper) {
-        modalBtnShow.addEventListener("click", showModal, false);
-        wrapper.addEventListener("click", hideModal, false);
-      }
+    if (modalBtnShow && wrapper) {
+        modalBtnShow.addEventListener('click', showModal, false);
+        wrapper.addEventListener('click', hideModal, false);
+        modalBtnHide.addEventListener('click', hideModal, false);
+    }
 
-      if (basket && wrapper) {
+    if (basket && wrapper) {
         basket.forEach(function(item) {
-          item.addEventListener("click", showModal, false);
+            item.addEventListener('click', showModal, false);
         });
-        wrapper.addEventListener("click", hideModal, false);
-      }
+        wrapper.addEventListener('click', hideModal, false);
+    }
 
 })();
-/*====================================================================
+/* ====================================================================
                                 SLIDER
 ======================================================================*/
 (function() {
-  // "use strict";
 
-  var isTrue = document.querySelector(".slider");
+    var isTrue = document.querySelector('.slider');
 
-  if (isTrue) { // Для работы слайдера только на главной
-    var slideNow = 1;
-    var sliderList = document.querySelector(".slider__list");
-    var slideCount = document.querySelector(".slider__list").children.length;
-    var next = document.querySelector(".slider__btn--next");
-    var prev = document.querySelector(".slider__btn--prev");
+    if (isTrue) { // Для работы слайдера только на главной
+        var slideNow = 1;
+        var sliderList = document.querySelector('.slider__list');
+        var slideCount = document.querySelector('.slider__list').children.length;
+        var next = document.querySelector('.slider__btn--next');
+        var prev = document.querySelector('.slider__btn--prev');
 
-    document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', function() {
 
-      function nextSlide() {
-          if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
-              sliderList.style.transform = "translate(0, 0)";
-              slideNow = 1;
-          } else {
-              translateWidth = -document.querySelector(".slider__viewport").offsetWidth * slideNow;
-              sliderList.style.transform = "translate("+ translateWidth + "px, 0)";
-              slideNow++;
-          }
-      };
+            function nextSlide() {
+                if (slideNow === slideCount || slideNow <= 0 || slideNow > slideCount) {
+                    sliderList.style.transform = 'translate(0, 0)';
+                    slideNow = 1;
+                } else {
+                    translateWidth = -document.querySelector('.slider__viewport').offsetWidth * slideNow;
+                    sliderList.style.transform = 'translate('+ translateWidth + 'px, 0)';
+                    slideNow++;
+                }
+            }
 
-      function prevSlide() {
-          if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
-              translateWidth = -document.querySelector(".slider__viewport").offsetWidth * (slideCount - 1);
-              sliderList.style.transform = "translate("+ translateWidth + "px, 0)";
-              slideNow = slideCount;
-          } else {
-              translateWidth = -document.querySelector(".slider__viewport").offsetWidth * (slideNow - 2);
-              sliderList.style.transform = "translate("+ translateWidth + "px, 0)";
-              slideNow--;
-          }
-      };
+            function prevSlide() {
+                if (slideNow === 1 || slideNow <= 0 || slideNow > slideCount) {
+                    translateWidth = -document.querySelector('.slider__viewport').offsetWidth * (slideCount - 1);
+                    sliderList.style.transform = 'translate('+ translateWidth + 'px, 0)';
+                    slideNow = slideCount;
+                } else {
+                    translateWidth = -document.querySelector('.slider__viewport').offsetWidth * (slideNow - 2);
+                    sliderList.style.transform = 'translate('+ translateWidth + 'px, 0)';
+                    slideNow--;
+                }
+            }
 
-        next.addEventListener("click", nextSlide, false);
-        prev.addEventListener("click", prevSlide, false);
+            next.addEventListener('click', nextSlide, false);
+            prev.addEventListener('click', prevSlide, false);
 
-    });
-  }
+        });
+    }
 
 })();
-/*--------------------------------------------------------------------------
+/* --------------------------------------------------------------------------
                               GOOGLE MAPS API
 -----------------------------------------------------------------------------*/
 var image = 'img/icon-map-pin.svg';
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 59.938631, lng: 30.323055}, //Координаты
-    zoom: 17
-  });
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 59.938631, lng: 30.323055}, // Координаты
+        zoom: 17
+    });
 
-  var beachMarker = new google.maps.Marker({
-    position: {lat: 59.938631, lng: 30.323055},
-    map: map,
-    title: "интернет-магазин Мишка",
-    icon: image
-  });
-};
-
-
-
-
+    var beachMarker = new google.maps.Marker({
+        position: {lat: 59.938631, lng: 30.323055},
+        map: map,
+        title: 'интернет-магазин Мишка',
+        icon: image
+    });
+}
